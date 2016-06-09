c60e332dcb9b9192ac9cc0a849787ebd614d2e6b
BlueCabbage <479680168@qq.com>
2016-06-09 01:49:26 -0700

    add Chapter13 until pipe2.c

---
+++ Chapter13/popen2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main()
+{
+	FILE *write_fp;
+	char buffer[BUFSIZ + 1];
+
+	sprintf(buffer, "Once upon a time, there was ...\n");
+	write_fp = popen("od -c", "w");
+	if (write_fp != NULL) {
+		fwrite(buffer, sizeof(char), strlen(buffer), write_fp);
+		pclose(write_fp);
+		exit(EXIT_SUCCESS);	
+	}
+	exit(EXIT_FAILURE);
+}
+
+
+
